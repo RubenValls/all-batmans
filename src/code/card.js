d8b1f07957dbce8deff5c59f483a3b581dbd096e
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2023-08-30 19:04:43 +0200

    Fix little bugs

--- src/code/card.js
+++ src/code/card.js
@@ -1,15 +1,24 @@
+const cards = Array.from(document.querySelectorAll('.card'))
 const infos = Array.from(document.getElementsByClassName('info'))
 const more = Array.from(document.getElementsByClassName('more'))
 
+const closeModalBtns = Array.from(document.querySelectorAll('.close-modal-btn'))
+
 more.forEach(anchor => {
-  anchor.addEventListener('click', (e) => {
-    const info = (e.target.parentElement.nextElementSibling)
+  anchor.addEventListener('click', () => {
+    const info = (anchor.parentElement.nextElementSibling)
     info.style.display = 'flex'
   })
 });
 
 infos.forEach(info => {
-  info.addEventListener('mouseleave', (e) => {
-    e.target.style.display = 'none'
+  info.addEventListener('mouseleave', () => {
+    info.style.display = 'none'
+  })
+})
+
+closeModalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.parentElement.style.display = 'none'
   })
 })
