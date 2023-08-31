b57a2c8f965664352e2c673fdd7766dd199d3e2e
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2023-08-31 20:08:33 +0200

    Fix mobile view

--- src/code/card.js
+++ src/code/card.js
@@ -1,30 +1,42 @@
 const cards = Array.from(document.querySelectorAll('.card'))
 const infos = Array.from(document.getElementsByClassName('info'))
-const more = Array.from(document.getElementsByClassName('more'))
+const mores = Array.from(document.getElementsByClassName('more'))
 
 const closeModalBtns = Array.from(document.querySelectorAll('.close-modal-btn'))
 
 cards.forEach(card => {
+  card.addEventListener('mouseleave', () => {
+    card.querySelector('.info').classList.remove('visible')
+  })
+
   card.addEventListener('touchend', (e) => {
-    console.log(e)
+    // console.log(e.target)
+    // console.log(e.currentTarget)
+
+    if(e.currentTarget.classList.contains('active')){
+      if(!e.target.classList.contains('more') && !e.target.parentElement.classList.contains('info')) {
+        e.currentTarget.classList.remove('active')
+      }
+    } else {
+      cards.forEach(card => {
+        card.classList.remove('active')
+        card.querySelector('.info').classList.remove('visible')
+      })
+
+      e.currentTarget.classList.add('active')
+    }
   })
 })
 
-more.forEach(anchor => {
-  anchor.addEventListener('click', () => {
-    const info = (anchor.parentElement.nextElementSibling)
-    info.style.display = 'flex'
-  })
-});
-
-infos.forEach(info => {
-  info.addEventListener('mouseleave', () => {
-    info.style.display = 'none'
+mores.forEach(more => {
+  more.addEventListener('click', (e) => {
+    e.preventDefault()
+    e.target.parentElement.parentElement.querySelector('.info').classList.add('visible')
   })
 })
 
-closeModalBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    btn.parentElement.style.display = 'none'
+closeModalBtns.forEach(closeBtn => {
+  closeBtn.addEventListener('click', (e) => {
+    e.currentTarget.parentElement.classList.remove('visible')
   })
 })
